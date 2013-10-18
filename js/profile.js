42c3430c671d7c47cf4cf72224129d6217e83b30
Julia Teitelbaum <julialt@gmail.com>
2013-10-18 01:39:32 -0400

    basic pie chart working. no labels, but hey

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,4 @@
-age_data = {
+var age_data = {
 	'breakdown': {
 		'under 17': 14.96,
 		'18 to 24': 4.46,
@@ -22,6 +22,14 @@ age_data = {
 window.onload = function() {
 	var r = Raphael("age");
 
+	var values = [age_data['breakdown']['under 17'],
+	 			  age_data['breakdown']['18 to 24'],
+	 			  age_data['breakdown']['25-64'],
+	 			  age_data['breakdown']['65 and over'],
+				 ];
+	r.piechart(320, 240, 150, values)
+
+/*
 	// Creates circle at x = 50, y = 40, with radius 10
 	var circle = r.circle(50, 40, 10);
 	// Sets the fill attribute of the circle to red (#f00)
@@ -29,4 +37,5 @@ window.onload = function() {
 
 	// Sets the stroke attribute of the circle to white
 	circle.attr("stroke", "#fff");
+*/
 }
