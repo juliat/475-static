d3face83f520574ca17a3f93d6433dc886de5833
Julia Teitelbaum <julialt@gmail.com>
2013-11-02 01:46:42 -0400

    just formatting tabs as spaces

--- js/router.js
+++ js/router.js
@@ -1,7 +1,7 @@
 /* This will tell Ember.js to detect when the application's URL matches '/' and to render the windfarms template. */
 App.Router.map(function () {
-	this.resource( 'windfarms' );
-	this.resource( 'windfarmAttributes');
+    this.resource( 'windfarms' );
+    this.resource( 'windfarmAttributes');
 });
 
  /* a WindfarmsRoute object with a model function that returns all the existing Windfarms */
